5e5290be5dbbcd4dbe62a61c80a6d0d40f6162e9
Yusnier Matos Arias <ymatos3@gmail.com>
2018-07-25 02:19:07 -0500

    oeducat_extras cambiar la generación de horarios para eliminar el asunto, permitir enerar con un solo día, mostrar un mensaje cuando no se ingresan planificaciones.

---
+++ wizard/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+import controllers
+import models
+from code import gen_time_table_line
---
+++ wizard/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class oeducat_attendance(models.Model):
+#     _name = 'oeducat_attendance.oeducat_attendance'
+
+#     name = fields.Char()
