25d0cac99d95ac8d943857caa56793ad1ff3e42f
yusniermatos <BitMyCode>
2019-03-12 12:52:10 -0500

    Exámenes en el formulario del estudiante

--- oeducat_exam/code/op_exam.py
+++ oeducat_exam/code/op_exam.py
@@ -25,26 +25,32 @@ from openerp import models, fields, api
 class OpExam(models.Model):
     _name = 'op.exam'
     _inherit = 'op.exam'
-    
+
     @api.one
     @api.onchange('division_id')
     def _compute_attendance_line(self):
         if self.division_id:
-            roll_number_ids = self.env['op.roll.number'].search([('division_id', '=', self.division_id.id), ('state', '=', 'active')]) # filtrar por activo
+            roll_number_ids = self.env['op.roll.number'].search(
+                [('division_id', '=', self.division_id.id), ('state', '=', 'active')])  # filtrar por activo
             data_lines = []
-            
+
             for rn in roll_number_ids:
                 data_lines.append((0, 0, {
                     'student_id': rn.student_id.id,
                     'exam_id': self._context.get('active_id', False),
                     'status': 'a'
                 }))
-            
+
             self.attendees_line = data_lines
-    
+
     attendees_line = fields.One2many(
         'op.exam.attendees', 'exam_id', 'Attendees', required=True)
-    #attendance_line = fields.One2many(
+    # attendance_line = fields.One2many(
     #    'op.attendance.line', 'attendance_id', 'Attendance Line',
     #    required=True)
-        
+
+
+class OpStudent(models.Model):
+    _inherit = 'op.student'
+
+    exam_attendance_ids = fields.One2many('op.exam.attendees', 'student_id', u'Exámenes')
