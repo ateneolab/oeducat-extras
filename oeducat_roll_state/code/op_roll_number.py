7922e1b5e5258b389111ce958435675b61ddfd7b
Erich Mario <emgomez@uci.cu>
2019-06-29 07:04:32 -0400

    Eliminar el onchange que actualiza el estado de la matrícula
    El estado de la matrícula cambia cuando se crea o actualiza
    un conjelamiento.

--- oeducat_roll_state/code/op_roll_number.py
+++ oeducat_roll_state/code/op_roll_number.py
@@ -25,66 +25,67 @@ from dateutil.relativedelta import relativedelta
 
 
 class OpRollNumber(models.Model):
-    _name = 'op.roll.number'
-    _inherit = 'op.roll.number'
+    _name = "op.roll.number"
+    _inherit = "op.roll.number"
 
     # state = fields.Selection(
     #     [('active', u'Asistiendo'), ('inactive', u'Congelado'), ('done', u'Incorporado'), ('gone', u'Retirado'), ('relocated', u'Trasladado')],
     #     default='active', string=u'Estado')
     state = fields.Selection(
-        [('active', u'Asistiendo'), ('inactive', u'Congelado'), ('done', u'Incorporado'), ('gone', u'Retirado'),
-         ('relocated', u'Cambio de escuela'), ('bochanged', u'Cambio de sucursal'),
-         ('bchange', u'Cambio de beneficiario'), ('inactive', u'No asiste')],
-        default='active', string=u'Estado')
-    date_state = fields.Datetime(u'Fecha de cambio de estado')
-    is_active = fields.Boolean(u'Activo', default=True)
-    previous_roll_number_id = fields.Many2one('op.roll.number', string=u'Matrícula anterior')
-    next_roll_number_id = fields.Many2one('op.roll.number', string=u'Matrícula nueva')
-
-    @api.onchange('freezing_ids')
-    def _onchange_freezing_ids(self):
-        if self.freezing_ids:
-            today = datetime.datetime.today()
-            for fi in self.freezing_ids:
-                start_date = datetime.datetime.strptime(fi.start_date, '%Y-%m-%d')
-                end_date = datetime.datetime.strptime(fi.end_date, '%Y-%m-%d')
-                if today >= start_date and today < end_date:
-                    self.frozen = True
-                    self.state = 'inactive'
-                    self.date_state = datetime.datetime.today()
+        [
+            ("active", u"Asistiendo"),
+            ("inactive", u"Congelado"),
+            ("done", u"Incorporado"),
+            ("gone", u"Retirado"),
+            ("relocated", u"Cambio de escuela"),
+            ("bochanged", u"Cambio de sucursal"),
+            ("bchange", u"Cambio de beneficiario"),
+            ("inactive", u"No asiste"),
+        ],
+        default="active",
+        string=u"Estado",
+    )
+    date_state = fields.Datetime(u"Fecha de cambio de estado")
+    is_active = fields.Boolean(u"Activo", default=True)
+    previous_roll_number_id = fields.Many2one(
+        "op.roll.number", string=u"Matrícula anterior"
+    )
+    next_roll_number_id = fields.Many2one("op.roll.number", string=u"Matrícula nueva")
 
     @api.multi
     def open_relocate_wizard(self):
-        wizard_form = self.env.ref('oeducat_roll_state.view_wizard_relocate_student_form', False)
-        view_id = self.env['op.wizard_relocate']
+        wizard_form = self.env.ref(
+            "oeducat_roll_state.view_wizard_relocate_student_form", False
+        )
+        view_id = self.env["op.wizard_relocate"]
         new = view_id.create({})
 
         return {
-            'name': "Trasladar estudiante",
-            'type': 'ir.actions.act_window',
-            'res_model': 'op.wizard_relocate',
-            'res_id': new.id,
-            'view_id': wizard_form.id,
-            'view_mode': 'form',
-            'view_type': 'form',
-            'nodestroy': True,
-            'target': 'new',
-            'context': {'default_current_roll_number_id': self.id}
+            "name": "Trasladar estudiante",
+            "type": "ir.actions.act_window",
+            "res_model": "op.wizard_relocate",
+            "res_id": new.id,
+            "view_id": wizard_form.id,
+            "view_mode": "form",
+            "view_type": "form",
+            "nodestroy": True,
+            "target": "new",
+            "context": {"default_current_roll_number_id": self.id},
         }
 
     @api.model
     def update(self, data):
-        id = data['id']
+        id = data["id"]
         # vals = data['vals']
         # print(id)
         # print(vals['roll_number'])
-        rn = self.env['op.roll.number'].browse([id])
+        rn = self.env["op.roll.number"].browse([id])
         # if rn:
         #     rn.write(vals)
         # print('ok')
 
-        seq = self.env['ir.sequence'].search([('code', '=', 'seq.roll.number')])
-        number = self.env['ir.sequence'].get(seq.code)
-        rn.write({'roll_number': number})
+        seq = self.env["ir.sequence"].search([("code", "=", "seq.roll.number")])
+        number = self.env["ir.sequence"].get(seq.code)
+        rn.write({"roll_number": number})
 
         return True
